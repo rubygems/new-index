e6c7e4da162a757a165ede62f0d2486c4a77c552
Andre Arko <andre@arko.net>
2013-09-26 11:08:15 -0700

    it's not actually an example per se

--- find_deps.rb
+++ find_deps.rb
@@ -1,5 +1,6 @@
 require_relative 'dep_group.rb'
 
 puts "Finding deps for #{ARGV[0]}..."
-dg = Bundler::DepGroup.new("rubygems", ARGV[0].dup)
+dg = Bundler::DepGroup.new(ARGV[0].dup)
+p dg.specs
 p dg.specs[ARGV[0]][ARGV[1]]
