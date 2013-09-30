c5367b26d957dd6e8dd4ccbcedb5ff7a308ba551
Andre Arko <andre@arko.net>
2013-09-30 16:57:30 -0700

    depend on bundler's new-index branch

--- find_deps.rb
+++ find_deps.rb
@@ -1,6 +1,7 @@
-require_relative 'dep_group.rb'
+$LOAD_PATH.unshift File.expand_path("~/src/bundler/bundler/lib")
+require 'bundler'
 
 puts "Finding deps for #{ARGV[0]}..."
-dg = Bundler::DepGroup.new(ARGV[0].dup)
-p dg.specs
-p dg.specs[ARGV[0]][ARGV[1]]
+ds = Bundler::DepSpecs.new("source", [ARGV[0].dup])
+p ds.spec_index
+p ds.spec_index.search Gem::Dependency.new(ARGV[0], ARGV[1])
